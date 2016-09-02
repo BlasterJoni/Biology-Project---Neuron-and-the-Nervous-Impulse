418582cd04c2a4d3e6fac4b539d49a501c59ad27
João Arvana <joaomanuel.arvana@gmail.com>
2016-09-02 15:20:07 +0100

    Update arduino.c

--- arduino.c
+++ arduino.c
@@ -8,12 +8,11 @@ void setup() {
 
 // Loop:
 // Ligths the 0 led and then enter the for loop.
-// In the for loop it ligths the next led and then shuts down the one before.
+// In the for loop it ligth the next led and then shuts down the one before.
 // When the the outputs number(variables a and b) gets over 9 it resets them to 0.
 void loop() {
   int a, b;
-  digitalWrite(0, HIGH);
-  for (a=0, b=1; true; a++, b++){
+  for (a=-1, b=0; true; a++, b++){
     if (a>9){
     	a=0;
     }
